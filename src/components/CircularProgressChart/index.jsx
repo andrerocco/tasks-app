43d7da94e3210ae8834d09297fb487ab6b40e3f8
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-01-12 21:00:36 -0300

    Criado componente 'CircularProgressChart'

---
+++ src/components/CircularProgressChart/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Svg, { Circle } from 'react-native-svg';
+import P from 'prop-types';
+
+export const CircularProgressChart = ({
+    progress,
+    diameter,
+    strokeWidth,
+    emptyColor = '#AEAEAE',
+    progressColor = '#5E5E5E',
+}) => {
+    return (
+        <Svg width={diameter} height={diameter} fill="none" xmlns="http://www.w3.org/2000/svg">
+            <Circle
+                cx={diameter / 2}
+                cy={diameter / 2}
+                r={diameter / 2 - strokeWidth / 2}
+                strokeWidth={strokeWidth}
+                stroke={emptyColor}
+            />
+            <Circle
+                cx={diameter / 2}
+                cy={diameter / 2}
+                r={diameter / 2 - strokeWidth / 2}
+                transform={`rotate(-90 ${diameter / 2} ${diameter / 2})`}
+                strokeWidth={strokeWidth}
+                stroke={progressColor}
+                strokeDasharray={`${parseInt(progress * 100)} 100`}
+            />
+        </Svg>
+    );
+};
+
+CircularProgressChart.propTypes = {
+    progress: P.number.isRequired,
+    diameter: P.number.isRequired,
+    strokeWidth: P.number.isRequired,
+    emptyColor: P.string,
+    progressColor: P.string,
+};
