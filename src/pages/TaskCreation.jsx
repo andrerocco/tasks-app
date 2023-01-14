af6fd2eeb2563baedc1d64b30a898f434e37ec90
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-01-14 20:38:50 -0300

    Criada pasta de páginas

---
+++ src/pages/TaskCreation.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export const TaskCreation = () => {
+    return (
+        <View style={{ flex: 1 }}>
+            <Text>TaskCreation</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
