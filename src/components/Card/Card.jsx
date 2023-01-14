8be3e32576d638b6e6e2883399c965da7ca57601
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-01-14 20:40:40 -0300

    Criado componente 'Card'

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import P from 'prop-types';
+// Constants
+import { COLORS } from '../../constants/colors.js';
+
+export const Card = ({ children, style }) => {
+    return <View style={{ ...styles.container, ...style }}>{children}</View>;
+};
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 'auto',
+        paddingVertical: 14,
+        paddingHorizontal: 4,
+        borderRadius: 9,
+        borderWidth: 1,
+        borderColor: COLORS.separator.withTransparency,
+        backgroundColor: COLORS.custom.cardBackground,
+    },
+});
+
+Card.propTypes = {
+    children: P.oneOfType([P.string, P.element, P.node]),
+    style: P.object,
+};
