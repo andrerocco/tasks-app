bad421a5121abc97f649fb857a3c6d1acb11b0cb
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-01-26 14:43:36 -0300

    Adicionados estilos de fontes

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -2,10 +2,67 @@ import { StyleSheet } from 'react-native';
 
 export const LABEL = StyleSheet.create({
     regular: {
+        captionSmall: {
+            fontSize: 11,
+            fontWeight: '400',
+            lineHeight: 13,
+        },
+        captionMedium: {
+            fontSize: 12,
+            fontWeight: '400',
+            lineHeight: 16,
+        },
+        footnote: {
+            fontSize: 13,
+            fontWeight: '400',
+            lineHeight: 18,
+        },
+        subheadline: {
+            fontSize: 15,
+            fontWeight: '400',
+            lineHeight: 20,
+        },
+        callout: {
+            fontSize: 16,
+            fontWeight: '400',
+            lineHeight: 21,
+        },
         body: {
             fontSize: 17,
             fontWeight: '400',
             lineHeight: 22,
+        },
+        headline: {
+            fontSize: 17,
+            fontWeight: '400',
+            lineHeight: 22,
+        },
+        title3: {
+            fontSize: 20,
+            fontWeight: '400',
+            lineHeight: 24,
+        },
+        title2: {
+            fontSize: 22,
+            fontWeight: '400',
+            lineHeight: 28,
+        },
+        title1: {
+            fontSize: 28,
+            fontWeight: '400',
+            lineHeight: 34,
+        },
+        largeTitle: {
+            fontSize: 34,
+            fontWeight: '400',
+            lineHeight: 41,
+        },
+    },
+    semibold: {
+        title1: {
+            fontSize: 28,
+            fontWeight: '500',
+            lineHeight: 34,
         },
     },
     bold: {
